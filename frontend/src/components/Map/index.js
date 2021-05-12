bfade7f04f5b120d90a3204ac149374ab4189ee3
Matheus Amaral <mam.macacod@gmail.com>
2021-05-12 18:58:37 -0300

    added api

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -2,24 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import React, {useState, useMapEvents} from 'react'
 import { Renderer } from 'leaflet'
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null)
-  const map = useMapEvents({
-    click() {
-      map.locate()
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
-    },
-  })
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  )
-}
+import api from '../../services/api';
 
 function Mapa () {
   const position = [[-15.833727, -48.057159], //tagua
